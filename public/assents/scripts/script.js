b12ca38c94778ac22e7990ed49be5adb3a841b48
KloseBH <danielhabdias@gmail.com>
2024-08-07 12:32:49 -0300

    Animações e Correções

--- public/assents/scripts/script.js
+++ public/assents/scripts/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    
+    $("#InfosBody, #ProjetosBody, .centered-item img" ).css("display", "none");
+
+    $("#Infos").mouseenter(function() {
+        var altura = $("#InfosBody");
+        $("#InfosBody").slideDown(2000); 
+    });
+
+    $("#Projetos").mouseenter(function() {
+        $("ProjetosBody").slideDown(2000);
+    });
+
+    $(".centered-item img").fadeIn(1000);
 });
+
